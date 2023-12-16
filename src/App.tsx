315ee171d0a2aaf666572af213a9d8c660a98d58
Lukas.J.Han <lukas.j.han@gmail.com>
2023-12-16 19:23:27 +0900

    Add sd jwt package for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import sdjwt from '@lukas.j.han/sd-jwt';
 
+function stringToUint8Array(str: string) {
+  const encoder = new TextEncoder();
+  const uint8Array = encoder.encode(str);
+  return uint8Array;
+}
+
+async function test() {
+  const claim = {
+    test: 'data',
+  };
+  const credentials = await sdjwt.issue(
+    claim,
+    stringToUint8Array('secret'),
+    {
+      _sd: ['test'],
+    },
+    {
+      sign_alg: 'HS256',
+    }
+  );
+  console.log(credentials);
+}
+test();
 function App() {
   return (
     <div className="App">
