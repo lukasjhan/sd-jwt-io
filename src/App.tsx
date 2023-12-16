c29253f65663c28db76f2a8927352f16bd9e0bce
Lukas.J.Han <lukas.j.han@gmail.com>
2023-12-16 23:39:11 +0900

    Revise title

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,25 @@ import NavBar from './components/NavBar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home } from './components/Pages/Home';
 import { Library } from './components/Pages/Library';
+import { Layout } from 'antd';
+const { Content, Footer } = Layout;
 
 function App() {
   return (
     <>
       <Router>
         <NavBar />
-
-        <div className="pages">
+        <Content style={{ marginTop: 64, minHeight: '100vh' }}>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/library" element={<Library />} />
           </Routes>
-        </div>
+        </Content>
+        <Footer
+          style={{ textAlign: 'center', background: 'black', color: 'white' }}
+        >
+          {'Footer content'}
+        </Footer>
       </Router>
     </>
   );
