c99a781922865186d4c6313587c2119f9be52936
Lukas.J.Han <lukas.j.han@gmail.com>
2023-12-28 13:03:13 +0900

    refactor: make hook

--- src/components/Debugger.tsx
+++ src/components/Debugger.tsx
@@ -4,17 +4,10 @@ import CodeMirror from 'codemirror';
 import { useEffect, useState } from 'react';
 import { Controlled as ControlledEditor } from 'react-codemirror2';
 import { CheckboxChangeEvent } from 'antd/es/checkbox';
-import sdjwt from '@hopae/sd-jwt';
 import { Warning } from './Warning';
-import {
-  copyCurrentURLToClipboard,
-  stringToUint8Array,
-  updateURLWithQuery,
-} from '../utils';
+import { copyCurrentURLToClipboard, updateURLWithQuery } from '../utils';
 import { SelectAlg } from './SelectAlg';
-
-const initialToken =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6InNkK2p3dCJ9.eyJsYXN0bmFtZSI6IkRvZSIsInNzbiI6IjEyMy00NS02Nzg5IiwiX3NkIjpbIk4yUXhZV1UxTlRnME1qQmpOR1JpWVRCaU1tRmtaamN5WXpSbFpXUmhaRGd5WkRCbE1qaGhZVGcwTnpJMU9XSXpZek5qWkdNNE1qZG1NVGN6TmpZd05RIiwiWlRSalkyUTVOemRoWkRVM05tWTFZV0UyTmpka01XVmpNRE16WXpOak5qQmtNak5pT0dZelpHSTBOelV4TURsak9EWTRNREEzWm1JeFpUY3daREZqTmciXSwiX3NkX2FsZyI6InNoYS0yNTYifQ.mX14Sw86xy8NFQta7tCfNmhVCqzfaJ_K3VEIhTjbLDY~WyJmYjlhZTU3OTMwMDE1NTA4NjY2YTQzODQwNGU1MzA1YiIsImZpcnN0bmFtZSIsIkpvaG4iXQ~WyJiOTYzMDcyNWViZjViYTE4OTc1ZWU4MWY4MWZkNTc3YyIsImlkIiwiMTIzNCJd~';
+import { DebugHook } from '../hooks/debug.hook';
 
 const JWTCode = ({
   token,
@@ -159,54 +152,28 @@ export const Debugger = () => {
     });
   }, []);
 
-  const [token, setToken] = useState(initialToken);
+  const {
+    token,
+    setToken,
+    secret,
+    setSecret,
+    base64Checked,
+    setBase64Checked,
+    discloseFrame,
+    setDiscloseFrame,
+    claims,
+    setClaims,
+    discolsures,
+    setDiscolsures,
+    header,
+    setHeader,
+    encode,
+    decode,
+    verify,
+  } = DebugHook();
+
   const [tab, setTab] = useState<'claim' | 'discloseFrame' | 'discolsures'>(
     'claim'
-  );
-
-  const [secret, setSecret] = useState('your-256-bit-secret');
-  const [base64Checked, setBase64Checked] = useState(false);
-
-  const [discloseFrame, setDiscloseFrame] = useState(
-    JSON.stringify(
-      {
-        _sd: ['firstname', 'id'],
-      },
-      null,
-      2
-    )
-  );
-
-  const [claims, setClaims] = useState(
-    JSON.stringify(
-      {
-        firstname: 'John',
-        lastname: 'Doe',
-        ssn: '123-45-6789',
-        id: '1234',
-      },
-      null,
-      2
-    )
-  );
-
-  const [discolsures, setDiscolsures] = useState(
-    JSON.stringify(
-      [
-        {
-          salt: 'fb9ae57930015508666a438404e5305b',
-          key: 'firstname',
-          value: 'John',
-        },
-        {
-          salt: 'b9630725ebf5ba18975ee81f81fd577c',
-          key: 'id',
-          value: '1234',
-        },
-      ],
-      null,
-      2
-    )
   );
 
   const tabValue = {
@@ -219,68 +186,6 @@ export const Debugger = () => {
     claim: setClaims,
     discloseFrame: setDiscloseFrame,
     discolsures: setDiscolsures,
-  };
-
-  const [header, setHeader] = useState(
-    JSON.stringify(
-      {
-        alg: 'HS256',
-        typ: 'sd+jwt',
-      },
-      null,
-      2
-    )
-  );
-
-  const encode = async () => {
-    try {
-      const data = JSON.parse(claims);
-      const sd_Data = JSON.parse(discloseFrame) as any;
-      console.log(data);
-      const token = await sdjwt.issue(
-        data,
-        stringToUint8Array(secret),
-        sd_Data,
-        {
-          sign_alg: 'HS256',
-        }
-      );
-      setToken(token);
-    } catch (e) {
-      console.error(e);
-      message.error('Encode Failed', 2);
-    }
-  };
-
-  const decode = async () => {
-    try {
-      const sdJwtToken = sdjwt.decode(token);
-      const header = JSON.stringify(sdJwtToken.jwt?.header ?? {}, null, 2);
-      const disclosures = JSON.stringify(sdJwtToken.disclosures ?? [], null, 2);
-      const claims = await sdjwt.getClaims(token);
-
-      setHeader(header);
-      setDiscolsures(disclosures);
-      setClaims(JSON.stringify(claims, null, 2));
-      setDiscloseFrame('');
-    } catch (e) {
-      console.error(e);
-      message.error('Decode Failed', 2);
-    }
-  };
-
-  const verify = async () => {
-    try {
-      const result = await sdjwt.validate(token, stringToUint8Array(secret));
-      if (result) {
-        message.success('Verify Success', 2);
-      } else {
-        message.error('Verify Failed', 2);
-      }
-    } catch (e) {
-      console.error(e);
-      message.error('Verify Failed', 2);
-    }
   };
 
   useEffect(() => {
---
+++ src/hooks/debug.hook.ts
@@ -0,0 +1,134 @@
+import { useState } from 'react';
+import { stringToUint8Array } from '../utils';
+import sdjwt from '@hopae/sd-jwt';
+import { message } from 'antd';
+
+const initialToken =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6InNkK2p3dCJ9.eyJsYXN0bmFtZSI6IkRvZSIsInNzbiI6IjEyMy00NS02Nzg5IiwiX3NkIjpbIk4yUXhZV1UxTlRnME1qQmpOR1JpWVRCaU1tRmtaamN5WXpSbFpXUmhaRGd5WkRCbE1qaGhZVGcwTnpJMU9XSXpZek5qWkdNNE1qZG1NVGN6TmpZd05RIiwiWlRSalkyUTVOemRoWkRVM05tWTFZV0UyTmpka01XVmpNRE16WXpOak5qQmtNak5pT0dZelpHSTBOelV4TURsak9EWTRNREEzWm1JeFpUY3daREZqTmciXSwiX3NkX2FsZyI6InNoYS0yNTYifQ.mX14Sw86xy8NFQta7tCfNmhVCqzfaJ_K3VEIhTjbLDY~WyJmYjlhZTU3OTMwMDE1NTA4NjY2YTQzODQwNGU1MzA1YiIsImZpcnN0bmFtZSIsIkpvaG4iXQ~WyJiOTYzMDcyNWViZjViYTE4OTc1ZWU4MWY4MWZkNTc3YyIsImlkIiwiMTIzNCJd~';
+
+export const DebugHook = () => {
+  const [token, setToken] = useState(initialToken);
+  const [secret, setSecret] = useState('your-256-bit-secret');
+  const [base64Checked, setBase64Checked] = useState(false);
+  const [discloseFrame, setDiscloseFrame] = useState(
+    JSON.stringify(
+      {
+        _sd: ['firstname', 'id'],
+      },
+      null,
+      2
+    )
+  );
+  const [claims, setClaims] = useState(
+    JSON.stringify(
+      {
+        firstname: 'John',
+        lastname: 'Doe',
+        ssn: '123-45-6789',
+        id: '1234',
+      },
+      null,
+      2
+    )
+  );
+  const [discolsures, setDiscolsures] = useState(
+    JSON.stringify(
+      [
+        {
+          salt: 'fb9ae57930015508666a438404e5305b',
+          key: 'firstname',
+          value: 'John',
+        },
+        {
+          salt: 'b9630725ebf5ba18975ee81f81fd577c',
+          key: 'id',
+          value: '1234',
+        },
+      ],
+      null,
+      2
+    )
+  );
+
+  const [header, setHeader] = useState(
+    JSON.stringify(
+      {
+        alg: 'HS256',
+        typ: 'sd+jwt',
+      },
+      null,
+      2
+    )
+  );
+
+  const encode = async () => {
+    try {
+      const data = JSON.parse(claims);
+      const sd_Data = JSON.parse(discloseFrame) as any;
+      console.log(data);
+      const token = await sdjwt.issue(
+        data,
+        stringToUint8Array(secret),
+        sd_Data,
+        {
+          sign_alg: 'HS256',
+        }
+      );
+      setToken(token);
+    } catch (e) {
+      console.error(e);
+      message.error('Encode Failed', 2);
+    }
+  };
+
+  const decode = async () => {
+    try {
+      const sdJwtToken = sdjwt.decode(token);
+      const header = JSON.stringify(sdJwtToken.jwt?.header ?? {}, null, 2);
+      const disclosures = JSON.stringify(sdJwtToken.disclosures ?? [], null, 2);
+      const claims = await sdjwt.getClaims(token);
+
+      setHeader(header);
+      setDiscolsures(disclosures);
+      setClaims(JSON.stringify(claims, null, 2));
+      setDiscloseFrame('');
+    } catch (e) {
+      console.error(e);
+      message.error('Decode Failed', 2);
+    }
+  };
+
+  const verify = async () => {
+    try {
+      const result = await sdjwt.validate(token, stringToUint8Array(secret));
+      if (result) {
+        message.success('Verify Success', 2);
+      } else {
+        message.error('Verify Failed', 2);
+      }
+    } catch (e) {
+      console.error(e);
+      message.error('Verify Failed', 2);
+    }
+  };
+
+  return {
+    token,
+    setToken,
+    secret,
+    setSecret,
+    base64Checked,
+    setBase64Checked,
+    discloseFrame,
+    setDiscloseFrame,
+    claims,
+    setClaims,
+    discolsures,
+    setDiscolsures,
+    header,
+    setHeader,
+    encode,
+    decode,
+    verify,
+  };
+};
