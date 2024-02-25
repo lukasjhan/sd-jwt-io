785eff2a1c0c37e7f67c6e069b61ec7711d2b694
Lukas.J.Han <lukas.j.han@gmail.com>
2024-02-25 15:47:39 +0900

    feat: remove unused alg
    Signed-off-by: Lukas.J.Han <lukas.j.han@gmail.com>

--- src/components/SelectAlgorithm.tsx
+++ src/components/SelectAlgorithm.tsx
@@ -17,18 +17,8 @@ export const SelectAlgorithm = () => {
 };
 
 const algorithms = [
-  { value: "HS256", label: "HS256" },
-  { value: "HS384", label: "HS384" },
-  { value: "HS512", label: "HS512" },
-  { value: "RS256", label: "RS256" },
-  { value: "RS384", label: "RS384" },
-  { value: "RS512", label: "RS512" },
-  { value: "ES256", label: "ES256" },
-  { value: "ES384", label: "ES384" },
-  { value: "ES512", label: "ES512" },
-  { value: "PS256", label: "PS256" },
-  { value: "PS384", label: "PS384" },
-  { value: "PS512", label: "PS512" },
+  { value: 'HS256', label: 'HS256' },
+  { value: 'ES256', label: 'ES256' },
 ];
 
 const SelectAlgorithmWrap = {
