d2700aba1e7c227eecd0e05b2616f569756e9a4c
tree <hhyjs2r@gmail.com>
2024-02-26 19:59:59 +0900

    feat: remove debounce
    Signed-off-by: tree <hhyjs2r@gmail.com>

--- src/components/encoded/JwtCode.tsx
+++ src/components/encoded/JwtCode.tsx
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from "react";
 import { Controlled as ControlledEditor } from "react-codemirror2";
 import { updateURLWithQuery } from "../../utils";
 
@@ -13,21 +12,6 @@ export const JwtCode = ({
   mode: string;
   decode: any;
 }) => {
-  // 디바운싱을 위한 상태와 타이머 변수
-  const [debouncedValue, setDebouncedValue] = useState(token);
-  let debounceTimer: NodeJS.Timeout;
-
-  useEffect(() => {
-    // token이 변경될 때마다 디바운싱을 위한 타이머를 재설정
-    clearTimeout(debounceTimer);
-    debounceTimer = setTimeout(() => {
-      // 일정 시간이 지난 후에 실제 처리를 수행
-      updateURLWithQuery(debouncedValue, mode);
-      setToken(debouncedValue);
-      decode();
-    }, 300); // 500ms 디바운싱 시간 설정
-  }, [debouncedValue, mode, setToken, decode]);
-
   return (
     <div className="area-wrapper">
       <ControlledEditor
@@ -37,7 +21,12 @@ export const JwtCode = ({
           lineWrapping: true,
           readOnly: mode === "decode",
         }}
-        onBeforeChange={(editor, data, value) => setDebouncedValue(value)}
+        onBeforeChange={(editor, data, value) => {
+          updateURLWithQuery(value, mode);
+          setToken(value);
+          decode();
+          // console.log(value);
+        }}
       />
     </div>
   );
