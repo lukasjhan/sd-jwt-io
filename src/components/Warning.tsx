e7171c4c8325feb2cdc14b0fde440f85c7160910
be bright <mail@jungmin.me>
2024-02-27 09:50:02 +0900

    Remove Close Button on Warning Message

--- src/components/Warning.tsx
+++ src/components/Warning.tsx
@@ -9,7 +9,6 @@ export const Warning = () => {
       showIcon
       message="SD-JWTs are credentials, which might have personal data. Be careful where you paste them! We do not record tokens, all validation and debugging is done on the client side."
       type="warning"
-      closable
     />
   );
 };
