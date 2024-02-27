0b6cd4f9fd109523b71f38741fff6455dcc04670
Lukas.J.Han <lukas.j.han@gmail.com>
2024-02-27 23:45:17 +0900

    chore: remove footer info
    Signed-off-by: Lukas.J.Han <lukas.j.han@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import "./App.module.css";
-import NavBar from "./components/NavBar";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Home } from "./components/Pages/Home";
-import { Library } from "./components/Pages/Library";
-import { Layout } from "antd";
-import { ContentWrapper } from "./components/ContentWrapper";
-import { GithubOutlined } from "@ant-design/icons";
+import './App.module.css';
+import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Home } from './components/Pages/Home';
+import { Library } from './components/Pages/Library';
+import { Layout } from 'antd';
+import { ContentWrapper } from './components/ContentWrapper';
+import { GithubOutlined } from '@ant-design/icons';
 const { Content, Footer } = Layout;
 
 function App() {
@@ -13,66 +13,32 @@ function App() {
     <>
       <Router>
         <NavBar />
-        <Content style={{ minHeight: "100vh" }}>
+        <Content style={{ minHeight: '100vh' }}>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/library" element={<Library />} />
           </Routes>
         </Content>
-        <Footer style={{ background: "black", color: "white" }}>
+        <Footer style={{ background: 'black', color: 'white' }}>
           <ContentWrapper>
             <div
               style={{
-                display: "flex",
-                justifyContent: "space-between",
+                display: 'flex',
+                justifyContent: 'space-between',
               }}
             >
-              <div>
-                <span
-                  style={{
-                    fontWeight: "bold",
-                  }}
-                >
-                  {"Missing something? "}
-                </span>
-                <span
-                  className="footer-link"
-                  onClick={() => {
-                    window.open(
-                      "https://github.com/lukasjhan/sd-jwt-io",
-                      "_blank"
-                    );
-                  }}
-                >
-                  {"Send a Pull Request"}
-                </span>
-                <span>{" - "}</span>
-                <span
-                  className="footer-link"
-                  onClick={() => {
-                    window.open(
-                      "https://github.com/lukasjhan/sd-jwt-io/blob/master/LICENSE.md",
-                      "_blank"
-                    );
-                  }}
-                >
-                  {"License"}
-                </span>
-              </div>
+              <div></div>
               <div
                 style={{
-                  cursor: "pointer",
+                  cursor: 'pointer',
                 }}
                 onClick={() => {
-                  window.open(
-                    "https://github.com/lukasjhan/sd-jwt-io",
-                    "_blank"
-                  );
+                  window.open('https://github.com/lukasjhan/sd-jwt-io', '_blank');
                 }}
               >
                 <GithubOutlined
                   style={{
-                    fontSize: "1.2rem",
+                    fontSize: '1.2rem',
                   }}
                 />
               </div>
