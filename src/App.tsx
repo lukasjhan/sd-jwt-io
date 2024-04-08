6ac763f2cc6642f5eab9eeb89418b259c6607d01
be bright <mail@jungmin.me>
2024-04-08 15:30:44 +0900

    Remove Route Tracker

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,12 @@ import { GithubOutlined } from '@ant-design/icons';
 import Decode from './components/Pages/Decode';
 import Issue from './components/Pages/Issue';
 import Present from './components/Pages/Present';
-import RouteTracker from './hooks/ga.hooks';
 
 const { Content, Footer } = Layout;
 function App() {
   return (
     <>
       <Router>
-        <RouteTracker />
         <NavBar />
         <Content style={{ minHeight: '100vh' }}>
           <Routes>
